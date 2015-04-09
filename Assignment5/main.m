d49b406ec070d9a6b40ca7f9c26226f0e2effacb
Yung Dai <yungchidai@gmail.com>
2015-04-09 13:45:06 -0400

    Class Level Init - Part 2

--- Assignment5/main.m
+++ Assignment5/main.m
@@ -19,6 +19,15 @@ int main(int argc, const char * argv[]) {
         toyota.model = @"Toyota Camry";
         NSLog(@"Changed the car to a %@", toyota.model);
         
+        
+        // Instantiating objects
+        Car *nissan = [[Car alloc] init];
+        NSLog(@"Created a %@", [nissan model]);
+        
+        Car *chevy = [[Car alloc] initWithModel:@"Chevy Corvette"];
+        NSLog(@"Created a %@, too.", chevy.model);
+        
+        
         [toyota drive];
         
         
