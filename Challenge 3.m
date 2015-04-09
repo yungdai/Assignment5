663b78a2e45c2db6967dc76dad7c51b6a313d717
Yung Dai <yungchidai@gmail.com>
2015-04-09 14:26:26 -0400

    Challenge 3 completed

---
+++ Challenge 3.m
@@ -0,0 +1,57 @@
+//
+//  Challenge 3.m
+//  Assignment5
+//
+//  Created by Yung Dai on 2015-04-09.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+/* Imagine that you own a boat and a motorcycle in addition to a car. List the actions you perform with each of these. Do you have any overlap between these actions? */
+
+// Boat
+// - drive
+// - steer
+// - refill
+// - clean
+// - ride
+// - tow
+// - start
+// - stop
+// - accellorate
+// - drain
+// - reverse
+// - load
+
+// Motorcycle
+//- drive
+//- steer
+// - refill
+// - clean
+// - ride
+// - start
+// - stop
+// - accellorate
+// - throttle
+// - break
+// - reverse
+
+// Car
+//- drive
+//- steer
+// - refill
+// - clean
+// - ride
+// - start
+// - stop
+// - accellorate
+// - throttle
+// - break
+// - reverse
+// - tow
+// - load
+
+// Yes there are overlapping actions
