98dae889b70b1c0f9c3744b1c59f1d2e559ce43d
Yung Dai <yungchidai@gmail.com>
2015-04-09 15:00:05 -0400

    Challenge 5 Completed

---
+++ Challenge 5.m
@@ -0,0 +1,13 @@
+//
+//  Challenge 5.m
+//  Assignment5
+//
+//  Created by Yung Dai on 2015-04-09.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//In a procedural language such as C, you think about actions and then write code to perform the action on various objects. Referring to the car example, you might write a procedure in C to wash a vehicle and then inside that procedure write code to handle washing a car, washing a boat, washing a motorcycle, and so on. If you took that approach and then wanted to add a new vehicle type (see the previous exercise), do you see advantages or disadvantages to using this procedural approach over an object- oriented approach?
+
+// Yes
