b0820ec94ec8e5c0be1b14eac66adf36c84023c8
Yung Dai <yungchidai@gmail.com>
2015-04-09 12:12:24 -0400

    Assignment 5
    Initial Commit

---
+++ Car.h
@@ -0,0 +1,17 @@
+//
+//  Car.h
+//  Assignment5
+//
+//  Created by Yung Dai on 2015-04-09.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@property (copy) NSString *model;
+
+-(void)drive;
+
+@end
---
+++ Car.m
@@ -0,0 +1,14 @@
+//
+//  Car.m
+//  Assignment5
+//
+//  Created by Yung Dai on 2015-04-09.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+
+@end
