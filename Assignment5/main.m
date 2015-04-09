c8efc342aa1096a7c20237e2e060f0b07d7c061c
Yung Dai <yungchidai@gmail.com>
2015-04-09 13:56:00 -0400

    Dynamic Typing

--- Assignment5/main.m
+++ Assignment5/main.m
@@ -30,6 +30,31 @@ int main(int argc, const char * argv[]) {
         
         [toyota drive];
         
+        Car *delorean = [[Car alloc]initWithModel:@"DeLorean"];
+        
+        // Get the class of an object
+        NSLog(@"%@ is an instance of the %@ class", [delorean model], [delorean class]);
+        
+        // check an object against a class and all subclasses
+        
+        if ([delorean isKindOfClass:[NSObject class]]) {
+            NSLog(@"%@ is an instance of NSObject or one of it's subclasses", [delorean model]);
+            
+        } else {
+            NSLog(@"%@ is not an instance of NSObject or one of it's subclasses", [delorean model]);
+        }
+        
+        // Check an object against a class, but not it's subclasses
+        if ([delorean isMemberOfClass:[NSObject class]]) {
+            NSLog(@"%@ is an instanance of NSObject", [delorean model]);
+        } else {
+            NSLog(@"%@ is not an instance of NSObject", [delorean model]);
+        }
+        
+        // Convert between strings and classes
+        if(NSClassFromString(@"Car") == [Car class]) {
+            NSLog(@"I can convert between strings and classes!");
+        }
         
     }
     return 0;
